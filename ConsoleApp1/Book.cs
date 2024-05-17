01912860ef43b2c437526a5bf095b2b6b6b23c5a
Maksonch1k <123887520+Maksonch1k@users.noreply.github.com>
2024-05-17 14:16:14 +0300

    Add files via upload

---
+++ ConsoleApp1/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Book
+    {
+        public string name;
+        public string fio;
+        public DateTime date;
+        public int kolvo;
+        public Book(string name, string fio, DateTime date, int kolvo)
+        {
+            this.name = name;
+            this.fio = fio;
+            this.date = date;
+            this.kolvo = kolvo;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"название{name}\nфио автора:{fio}\nдата издания{date.ToShortDateString()}\nколво страниц{kolvo}");
+            
+        }
+        public static Book Enter()
+        {
+            Console.WriteLine("ведите название");
+            string name = Console.ReadLine();
+            Console.WriteLine("введите фио автора");
+            string fio = Console.ReadLine();
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Введите колво страниц");
+            int kolvo = Convert.ToInt32(Console.ReadLine());
+            return new Book(name, fio, date, kolvo);
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void ClassTaker(Class1 class1)
+        {
+            class1.change = "изменено";
+        }
+        static void StructTaker(MyStruct myStruct)
+        {
+            myStruct.change = "изменено";
+        }
+        static void Main(string[] args)
+        {
+            //Class1 class1 = new Class1();
+            //class1.change = "не изменено";
+
+            //MyStruct myStruct = new MyStruct();
+            //myStruct.change = "не изменено";
+
+            //Console.WriteLine("исход. знач");
+            //Console.WriteLine("class1.change " + class1.change);
+            //Console.WriteLine("myStruct.change " + myStruct.change);
+            //Console.WriteLine();
+
+            //ClassTaker(class1);
+            //StructTaker(myStruct);
+
+            //Console.WriteLine("после вызова методов");
+            //Console.WriteLine("Class1.change " + class1.change);
+
+            //Notebook notebook = new Notebook("Notebook X", "ABC Company", 400);
+            //notebook.DisplayInfo();
+            List<Book> list = new List<Book>();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine($"1-добавление элемента\n 2 - выход всех элементов\n 3 - выход");
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case '1':
+                        list.Add(Book.Enter());
+                        break;
+                    case '2':
+                        foreach (var elem in list)
+                        {
+                            elem.Print();
+                        }
+                        break;
+                    case '3': return;
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
